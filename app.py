40088fb0d2f61bb0e15ce5c4937367b11a47d8ab
digipri-22 <prinka@usf.edu>
2025-11-23 23:03:15 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
 print("Welcome to my Python program!")
 kitties = input("How many kitties do you have?")
+kitties=int(kitties)
+kittyFoodperDay=kitties*3
+print(f"You need {kittyFoodperDay} servings of kitty food per day!")
