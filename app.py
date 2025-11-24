42426ee529a2c35fda91cd279a26bad302573a30
digipri-22 <prinka@usf.edu>
2025-11-23 23:04:29 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 print("Welcome to my Python program!")
 kitties = input("How many kitties do you have?")
-kitties=int(kitties)
+try:
+    kitties=int(kitties)
+except ValueError:
+    print("Please enter a valid number.")
+    kitties = input("How many kitties do you have?")
+    exit()
 kittyFoodperDay=kitties*3
 print(f"You need {kittyFoodperDay} servings of kitty food per day!")
